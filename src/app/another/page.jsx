2f75690b9e11a7b951059dabf0306eda13118813
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2023-11-09 21:54:35 +0600

    final done

--- src/app/another/page.jsx
+++ src/app/another/page.jsx
@@ -10,7 +10,7 @@ const AnotherPage = () => {
           <img src="/image 1.svg" className=" object-contain" alt="" />
           <p className="font-semibold text-lg">This page is empty</p>
           <a
-            className="inline-block rounded-lg bg-orange-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:bg-orange-600"
+            className="inline-block rounded-lg bg-orange-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:bg-orange-600 cursor-pointer"
             onClick={() => router.push("/")}
           >
             👈🏻 Go to Home
